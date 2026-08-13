1948ae843df04311872981f24de30c5493fe2b87
Rushil-0811 <rushil1108@gmail.com>
2026-08-13 23:20:44 +0530

    Med 3

--- Arrays/Kadane'sAlgo.py
+++ Arrays/Kadane'sAlgo.py
@@ -99,3 +99,43 @@ if __name__ == "__main__":
 # If at any point the sum becomes negative, reset it to 0, as a negative sum won't contribute positively to the overall maximum sum.
 # Continue the iteration until all elements in the array are processed.
 # Finally, return the maximum sum encountered during the iteration.
+
+from typing import List
+
+class Solution:
+    # Function to find maximum sum of subarrays
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        # maximum sum
+        maxi = float('-inf') 
+        
+        # current sum of subarray
+        sum = 0 
+        
+        # Iterate through the array
+        for i in range(len(nums)):
+            
+            # Add current element to the sum
+            sum += nums[i] 
+            
+            # Update maxi if current sum is greater
+            if sum > maxi:
+                maxi = sum 
+            
+            # Reset sum to 0 if it becomes negative
+            if sum < 0:
+                sum = 0 
+        
+        # Return the maximum subarray sum found
+        return maxi
+
+if __name__ == "__main__":
+    arr = [ -2, 1, -3, 4, -1, 2, 1, -5, 4 ]
+
+    # Create an instance of Solution class
+    sol = Solution()
+
+    maxSum = sol.maxSubArray(arr)
+
+    # Print the max subarray sum
+    print(f"The maximum subarray sum is: {maxSum}")
